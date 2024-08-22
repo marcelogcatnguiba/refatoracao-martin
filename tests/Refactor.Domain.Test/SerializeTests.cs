b48bb76b22f771cdcfe93e4502402b08c6c6fc94
marcelo.gomes <marcelo.gomes@microdatasistemas.com.br>
2024-08-22 10:43:17 -0300

    refatora tests de plays invoices

--- tests/Refactor.Domain.Test/SerializeTests.cs
+++ tests/Refactor.Domain.Test/SerializeTests.cs
@@ -1,47 +1,104 @@
-using System.Text.Json;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using Refactor.Domain.Entities;
 
 namespace Refactor.Domain.Test
 {
-    public class SerializeTests
+    public class SerializeTests : IDisposable
     {
-        [Fact]
-        public void DeveDeserializarUmaListaDePlays()
+        private readonly string _caminhoPlays;
+        private readonly string _caminhoInvoices;
+        public SerializeTests()
         {
-            var json = File.ReadAllText(@"..\..\..\..\JSON\plays.json");
+            string plays = 
+            """
+                [
+                    {
+                        "name": "Hamlet",
+                        "type": "tragedy"
+                    },
+                    {
+                        "name": "As You Like It",
+                        "type": "comedy"
+                    },
+                    {
+                        "name": "Othello",
+                        "type": "tragedy"
+                    }
+                ]
+            """;
+
+            string invoices = 
+            """
+            {
+                "customer": "BigCo",
+                "performances": [
+                    {
+                        "playID": "Hamlet",
+                        "audience": 55
+                    },
+                    {
+                        "playID": "As You Like It",
+                        "audience": 35
+                    },
+                    {
+                        "playID": "Othello",
+                        "audience": 40
+                    }
+                ]
+            }
+            """;
+
+            File.WriteAllText("plays.json", plays);
+            File.WriteAllText("invoices.json", invoices);
+            
+            _caminhoInvoices = Path.GetFullPath("invoices.json");
+            _caminhoPlays = Path.GetFullPath("plays.json");
+        }
+
+        [Fact]
+        public void DeveDeserializar_Plays()
+        {
+            var json = File.ReadAllText("plays.json");
             var result = JsonConvert.DeserializeObject<List<Plays>>(json);
 
             Assert.IsType<List<Plays>>(result);
         }
 
         [Fact]
-        public void DeveConterDadosEmPlays()
+        public void DeveConterDados_Plays()
         {
-            var json = File.ReadAllText(@"..\..\..\..\JSON\plays.json");
+            var json = File.ReadAllText("plays.json");
             var result = JsonConvert.DeserializeObject<List<Plays>>(json);
 
             Assert.NotEmpty(result!);
         }
 
         [Fact]
-        public void DeveDeserializarUmaInvoice()
+        public void DeveDeserializar_Invoice()
         {
-            var json = File.ReadAllText(@"..\..\..\..\JSON\invoices.json");
+            var json = File.ReadAllText("invoices.json");
             var result = JsonConvert.DeserializeObject<Invoices>(json);
 
             Assert.IsType<Invoices>(result);
         }
 
         [Fact]
-        public void DeveConterDadosNoInvoices()
+        public void DeveConterDados_Invoices()
         {
-            var json = File.ReadAllText(@"..\..\..\..\JSON\invoices.json");
+            var json = File.ReadAllText("invoices.json");
             var result = JsonConvert.DeserializeObject<Invoices>(json);
 
             Assert.Equal("BigCo", result!.Customer);
             Assert.NotEmpty(result!.Performances);
+        }
+
+        public void Dispose()
+        {
+            File.Delete(_caminhoPlays);
+            File.Delete(_caminhoInvoices);
+
+            //Para nao invocar o GC novamente pois eu ja limpei ele da memoria aqui
+            GC.SuppressFinalize(this);
         }
     }
 }
